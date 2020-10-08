0c368881015aad2dd2ee4f929918e2c9b6298696
Damian McGregor <mcgregor.damian@gmial.com>
2020-10-07 22:36:07 -0700

    Initial assignment upload.

---
+++ cse360assignment01/src/cse360assignment01/AddingMachine.java
@@ -0,0 +1,56 @@
+/**
+ * CSE 360 Fall 2020 Assignment 1
+ * @author Damian McGregor
+ * Description:	AddingMachine takes given input values and performs method defined operations,
+ * 				including addition, subtraction, provide total, display history, and clear history.
+ */
+
+package cse360assignment01;
+
+
+public class AddingMachine {
+	private int total;
+	  
+	/**
+	 * Constructor for AddingMachine class
+	 */
+	  public AddingMachine () {
+	    total = 0;  // not needed - included for clarity
+	  }
+	  
+	  /**
+	   * returns current total
+	   * @return total
+	   */
+	  public int getTotal () {
+	    return 0;
+	  }
+	  
+	  /**
+	   * adds value to total
+	   * @param value
+	   */
+	  public void add (int value) {
+	  }
+
+	  /**
+	   * subtracts value from total
+	   * @param value
+	   */
+	  public void subtract (int value) {
+	  }
+
+	  /**
+	   * display transaction history
+	   * @return history
+	   */
+	  public String toString () {
+	    return "";
+	  }
+
+	  /**
+	   * clear AddingMachine
+	   */
+	  public void clear() {
+	  }
+}
